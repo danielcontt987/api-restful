831db4f3aedcd4438b34d6d76c1663ea6c1e1a73
danielcontt987 <josedanielcontrerasperez510@gmail.com>
2021-09-08 21:20:48 -0600

    Actualizacion de rutas

--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,10 @@
 <?php
 
+use App\Http\Controllers\ArticuloController;
 use App\Http\Controllers\ArticulosController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/articulos', 'App\Http\Controllers\ArticuloController@index'); //mostrar todos los registros
-Route::post('/articulos', 'App\Http\Controllers\ArticuloController@store'); //crear un registro
-Route::put('/articulos/{id}', 'App\Http\Controllers\ArticuloController@update'); //actualizar un registro
-Route::delete('/articulos/{id}', 'App\Http\Controllers\ArticuloController@destroy'); //actualizar un registro
+Route::get('/articulos', [ArticuloController::class, 'index']); //mostrar todos los registros
+Route::post('/articulos', [ArticuloController::class, 'store']); //crear un registro
+Route::put('/articulos/{id}', [ArticuloController::class, 'update']); //actualizar un registro
+Route::delete('/articulos/{id}', [ArticuloController::class, 'destroy']); //actualizar un registro
